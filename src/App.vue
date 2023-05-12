7004700b85274d1e27d4ad10750c877f6eef7d9c
kuacoco <kuacoco@qq.com>
2023-05-12 10:28:06 +0800

    响应式显示卡片

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,180 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+    mounted() {
+        let vh = window.innerHeight * 0.01
+        document.documentElement.style.setProperty('--vh', `${vh}px`)
+        window.addEventListener('resize', () => {
+            let vh = window.innerHeight * 0.01
+            document.documentElement.style.setProperty('--vh', `${vh}px`)
+        })
+    },
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <el-container>
+        <el-header class="header"></el-header>
+        <el-main class="main">
+            <el-card class="card">
+                <el-image fit="contain"
+                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                    class="image" />
+                <div style="padding: 14px">
+                    <span>Yummy hamburger</span>
+                    <div class="bottom">
+                        <time class="time">2019-1-1</time>
+                        <el-button text class="button">Operating</el-button>
+                    </div>
+                </div>
+            </el-card>
+            <el-card class="card">
+                <el-image fit="contain"
+                    src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/3f98889a99304418934d94a0ef0dc3c5~tplv-k3u1fbpfcp-zoom-in-crop-mark:4536:0:0:0.awebp?"
+                    class="image" />
+                <div style="padding: 14px">
+                    <span>Yummy hamburger</span>
+                    <div class="bottom">
+                        <time class="time">2019-1-1</time>
+                        <el-button text class="button">Operating</el-button>
+                    </div>
+                </div>
+            </el-card>
+            <el-card class="card">
+                <el-image fit="contain"
+                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                    class="image" />
+                <div style="padding: 14px">
+                    <span>Yummy hamburger</span>
+                    <div class="bottom">
+                        <time class="time">2019-1-1</time>
+                        <el-button text class="button">Operating</el-button>
+                    </div>
+                </div>
+            </el-card>
+            <el-card class="card">
+                <el-image fit="contain"
+                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                    class="image" />
+                <div style="padding: 14px">
+                    <span>Yummy hamburger</span>
+                    <div class="bottom">
+                        <time class="time">2019-1-1</time>
+                        <el-button text class="button">Operating</el-button>
+                    </div>
+                </div>
+            </el-card>
+            <el-card class="card">
+                <el-image fit="contain"
+                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                    class="image" />
+                <div style="padding: 14px">
+                    <span>Yummy hamburger</span>
+                    <div class="bottom">
+                        <time class="time">2019-1-1</time>
+                        <el-button text class="button">Operating</el-button>
+                    </div>
+                </div>
+            </el-card>
+            <el-card class="card">
+                <el-image fit="contain"
+                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                    class="image" />
+                <div style="padding: 14px">
+                    <span>Yummy hamburger</span>
+                    <div class="bottom">
+                        <time class="time">2019-1-1</time>
+                        <el-button text class="button">Operating</el-button>
+                    </div>
+                </div>
+            </el-card>
+            <el-card class="card">
+                <el-image fit="contain"
+                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                    class="image" />
+                <div style="padding: 14px">
+                    <span>Yummy hamburger</span>
+                    <div class="bottom">
+                        <time class="time">2019-1-1</time>
+                        <el-button text class="button">Operating</el-button>
+                    </div>
+                </div>
+            </el-card>
+            <el-card class="card">
+                <el-image fit="contain"
+                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                    class="image" />
+                <div style="padding: 14px">
+                    <span>Yummy hamburger</span>
+                    <div class="bottom">
+                        <time class="time">2019-1-1</time>
+                        <el-button text class="button">Operating</el-button>
+                    </div>
+                </div>
+            </el-card>
+            <el-card class="card">
+                <el-image fit="contain"
+                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                    class="image" />
+                <div style="padding: 14px">
+                    <span>Yummy hamburger</span>
+                    <div class="bottom">
+                        <time class="time">2019-1-1</time>
+                        <el-button text class="button">Operating</el-button>
+                    </div>
+                </div>
+            </el-card>
+            <el-card class="card">
+                <el-image fit="contain"
+                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                    class="image" />
+                <div style="padding: 14px">
+                    <span>Yummy hamburger</span>
+                    <div class="bottom">
+                        <time class="time">2019-1-1</time>
+                        <el-button text class="button">Operating</el-button>
+                    </div>
+                </div>
+            </el-card>
+        </el-main>
+        <el-footer class="header">Footer</el-footer>
+    </el-container>
 </template>
-
+  
 <style scoped>
-header {
-  line-height: 1.5;
+.header {
+    background-color: pink;
+    height: 3.5rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+.main {
+    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100 - 7rem);
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
     flex-wrap: wrap;
-  }
+    justify-content: space-evenly;
+    padding: 1vh 1vw;
+}
+
+.card {
+    margin-bottom: 1vh;
+    width: 90vw;
+}
+
+@media (min-width:350px) {
+    .card {
+        width: 45vw;
+    }
+}
+
+@media screen and (min-width:768px) {
+    .card {
+        width: 23vw;
+    }
+}
+
+@media screen and (min-width:1024px) {
+    .card {
+        width: 15vw;
+    }
 }
 </style>
