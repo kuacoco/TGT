191ef09f72f06306986e2452c6bd89b74d9c25d7
kuacoco <kuacoco@qq.com>
2023-05-15 15:45:00 +0800

    cardstyle

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script>
 export default {
+    data() {
+        return {
+            badyStyle: {
+                padding: 0,
+                height: '100%',
+            }
+        }
+    },
     mounted() {
         let vh = window.innerHeight * 0.01
         document.documentElement.style.setProperty('--vh', `${vh}px`)
@@ -20,135 +28,79 @@ export default {
             </span>
         </el-header>
         <el-main class="main">
-            <el-card class="card">
-                <el-image fit="contain"
+            <el-card class="card" :body-style="badyStyle">
+                <el-image fit="cover"
                     src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
                     class="image" />
-                <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                        <time class="time">2019-1-1</time>
-                        <el-button text class="button">Operating</el-button>
-                    </div>
+                <div class="text">
+                    <span>美味的汉堡包 美味的汉堡包 美味的汉堡包 美味的汉堡包 美味的汉堡包</span>
                 </div>
             </el-card>
-            <el-card class="card">
-                <el-image fit="contain"
+            <el-card class="card" :body-style="badyStyle">
+                <el-image fit="cover"
                     src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/3f98889a99304418934d94a0ef0dc3c5~tplv-k3u1fbpfcp-zoom-in-crop-mark:4536:0:0:0.awebp?"
                     class="image" />
-                <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                        <time class="time">2019-1-1</time>
-                        <el-button text class="button">Operating</el-button>
-                    </div>
+                <div class="text">
+                    <span>难吃的汉堡包 难吃的汉堡包 难吃的汉堡包 难吃的汉堡包 难吃的汉堡包</span>
                 </div>
             </el-card>
-            <el-card class="card">
-                <el-image fit="contain"
+            <el-card class="card" :body-style="badyStyle">
+                <el-image fit="cover"
                     src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
                     class="image" />
-                <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                        <time class="time">2019-1-1</time>
-                        <el-button text class="button">Operating</el-button>
-                    </div>
+                <div class="text">
+                    <span>美味的汉堡包 美味的汉堡包 美味的汉堡包 美味的汉堡包 美味的汉堡包</span>
                 </div>
             </el-card>
-            <el-card class="card">
-                <el-image fit="contain"
-                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            <el-card class="card" :body-style="badyStyle">
+                <el-image fit="cover"
+                    src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/3f98889a99304418934d94a0ef0dc3c5~tplv-k3u1fbpfcp-zoom-in-crop-mark:4536:0:0:0.awebp?"
                     class="image" />
-                <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                        <time class="time">2019-1-1</time>
-                        <el-button text class="button">Operating</el-button>
-                    </div>
+                <div class="text">
+                    <span>难吃的汉堡包 难吃的汉堡包 难吃的汉堡包 难吃的汉堡包 难吃的汉堡包难吃的汉堡包 难吃的汉堡包 难吃的汉堡包 难吃的汉堡包 难吃的汉堡包</span>
                 </div>
             </el-card>
-            <el-card class="card">
-                <el-image fit="contain"
+            <el-card class="card" :body-style="badyStyle">
+                <el-image fit="cover"
                     src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
                     class="image" />
-                <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                        <time class="time">2019-1-1</time>
-                        <el-button text class="button">Operating</el-button>
-                    </div>
+                <div class="text">
+                    <span>美味的汉堡包 美味的汉堡包 美味的汉堡包 美味的汉堡包 美味的汉堡包美味的汉堡包 美味的汉堡包 美味的汉堡包 美味的汉堡包 美味的汉堡包</span>
                 </div>
             </el-card>
-            <el-card class="card">
-                <el-image fit="contain"
-                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            <el-card class="card" :body-style="badyStyle">
+                <el-image fit="cover"
+                    src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/3f98889a99304418934d94a0ef0dc3c5~tplv-k3u1fbpfcp-zoom-in-crop-mark:4536:0:0:0.awebp?"
                     class="image" />
-                <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                        <time class="time">2019-1-1</time>
-                        <el-button text class="button">Operating</el-button>
-                    </div>
+                <div class="text">
+                    <span>难吃的汉堡包 难吃的汉堡包 难吃的汉堡包 难吃的汉堡包 难吃的汉堡包</span>
                 </div>
             </el-card>
-            <el-card class="card">
-                <el-image fit="contain"
+            <el-card class="card" :body-style="badyStyle">
+                <el-image fit="cover"
                     src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
                     class="image" />
-                <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                        <time class="time">2019-1-1</time>
-                        <el-button text class="button">Operating</el-button>
-                    </div>
+                <div class="text">
+                    <span>美味的汉堡包 美味的汉堡包 美味的汉堡包 美味的汉堡包 美味的汉堡包</span>
                 </div>
             </el-card>
-            <el-card class="card">
-                <el-image fit="contain"
-                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            <el-card class="card" :body-style="badyStyle">
+                <el-image fit="cover"
+                    src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/3f98889a99304418934d94a0ef0dc3c5~tplv-k3u1fbpfcp-zoom-in-crop-mark:4536:0:0:0.awebp?"
                     class="image" />
-                <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                        <time class="time">2019-1-1</time>
-                        <el-button text class="button">Operating</el-button>
-                    </div>
-                </div>
-            </el-card>
-            <el-card class="card">
-                <el-image fit="contain"
-                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-                    class="image" />
-                <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                        <time class="time">2019-1-1</time>
-                        <el-button text class="button">Operating</el-button>
-                    </div>
-                </div>
-            </el-card>
-            <el-card class="card">
-                <el-image fit="contain"
-                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-                    class="image" />
-                <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                        <time class="time">2019-1-1</time>
-                        <el-button text class="button">Operating</el-button>
-                    </div>
+                <div class="text">
+                    <span>难吃的汉堡包 难吃的汉堡包 难吃的汉堡包 难吃的汉堡包 难吃的汉堡包</span>
                 </div>
             </el-card>
         </el-main>
         <el-footer class="footer">
-            <span class="link" style="color:#b88230;">
+            <span class="navigation" style="color:#b88230;">
                 <el-icon class="icon">
                     <HomeFilled />
                 </el-icon>
                 <span>首页</span>
             </span>
-            <span class="link">
+            <span class="navigation">
                 <el-icon class="icon">
                     <UserFilled />
                 </el-icon>
@@ -178,7 +130,7 @@ export default {
     font-size: x-large;
 }
 
-.link {
+.navigation {
     font-size: 0.6rem;
     text-align: center;
     display: flex;
@@ -203,25 +155,43 @@ export default {
 }
 
 .card {
-    margin-bottom: 1vh;
-    width: 90vw;
-}
-
-@media (min-width:350px) {
-    .card {
-        width: 45vw;
-    }
+    margin-bottom: 3vh;
+    height: 30vh;
+    width: 80vw;
 }
 
 @media screen and (min-width:768px) {
     .card {
-        width: 23vw;
+        width: 40vw;
     }
 }
 
 @media screen and (min-width:1024px) {
     .card {
-        width: 15vw;
+        width: 14vw;
     }
+}
+
+.image {
+    height: 20vh;
+    width: 100%;
+    display: block;
+}
+
+.text {
+    padding: 0 10px;
+    height: 10vh;
+}
+
+.text span {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    /*! autoprefixer: off */
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
 }
 </style>
